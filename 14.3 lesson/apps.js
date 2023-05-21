81621c22beeab14586f37861b9c6d9d6ad5b589a
Miguel <miguel.a.v90@gmail.com>
2023-05-21 16:25:48 -0700

    14.3 quiz

---
+++ 14.3 lesson/apps.js
@@ -0,0 +1,23 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+const PI = 3.14
+
+/* Write an ES2015 Version */
+
+// What is the difference between ***var*** and ***let***?
+A var can be reassigned, redeclared, mutate and it's a function scope.
+A let can be reassigned and can be mutated, but it cannot be redeclread and it works in a block scope.
+
+// What is the difference between **var** and ***const***?
+
+Const cannot be reassigned, redeclared, but can be mutated. It is also a block scope. 
+var can be reassigned, redeclared, mutate and it's a function scope.
+
+// What is the difference between **let** and **const**?
+    
+    const is close to let but it cannot be reassigned. 
+
+// What is hoisting?
+
+It's when the declaration moves to the top, such as Var. And this is before the execution of the code. 
